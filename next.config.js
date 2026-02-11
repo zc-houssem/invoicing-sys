c870918b2ad12a494ee5cb6aa8a865662398be77
softsurgery <109615722+softsurgery@users.noreply.github.com>
2026-02-11 11:05:16 +0100

    build: update nextjs config & tsconfig

--- next.config.js
+++ next.config.js
@@ -1,12 +1,10 @@
-const { i18n } = require('./next-i18next.config.js');
 /** @type {import('next').NextConfig} */
+const { i18n } = require('./next-i18next.config.js');
 const nextConfig = {
   output: 'standalone',
   reactStrictMode: true,
   i18n,
-  compiler: {
-    styledComponents: true
-  }
+
 };
 
 module.exports = nextConfig;
