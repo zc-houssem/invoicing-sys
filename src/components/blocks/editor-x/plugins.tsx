4c57ca3f9910914c9107d286e95548d5ac98d02d
softsurgery <109615722+softsurgery@users.noreply.github.com>
2026-03-25 11:43:52 +0100

    fix: normalize editor height

--- src/components/blocks/editor-x/plugins.tsx
+++ src/components/blocks/editor-x/plugins.tsx
@@ -172,7 +172,7 @@ export function Plugins({}) {
               <div className="" ref={onRef}>
                 <ContentEditable
                   placeholder={placeholder}
-                  className="ContentEditable__root relative block h-[calc(100vh-90px)] min-h-72 overflow-auto px-8 py-4 focus:outline-none"
+                  className="ContentEditable__root relative block min-h-72 overflow-auto px-8 py-4 focus:outline-none"
                 />
               </div>
             </div>
