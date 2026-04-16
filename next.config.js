915a0fcafe2dfdcb97b1cacf4e423cd792cf2e4b
softsurgery <109615722+softsurgery@users.noreply.github.com>
2026-04-16 17:38:26 +0100

    build: update to next 16

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
+
 const { i18n } = require('./next-i18next.config.js');
 const nextConfig = {
   output: 'standalone',
   reactStrictMode: true,
   i18n,
-
+  turbopack: {},
 };
 
 module.exports = nextConfig;
