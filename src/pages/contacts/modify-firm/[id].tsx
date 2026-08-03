6df2ea202b5753e4c9b524a3033fac767e52db05
softsurgery <153816533+zc-houssem@users.noreply.github.com>
2026-08-03 22:22:22 +0100

    chore: repair various roures for enterprise , quotation & payment

--- src/pages/contacts/modify-firm/[id].tsx
+++ src/pages/contacts/modify-firm/[id].tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useRouter } from 'next/router';
-import { FirmUpdateForm } from '@/components/contacts/enterprise/FirmUpdateForm';
+import { EnterpriseUpdateForm } from '@/components/contacts/enterprise/form/EnterpriseUpdateForm';
 import { Page404 } from '@/components/shared';
 
 export default function Page() {
@@ -10,7 +10,7 @@ export default function Page() {
   if (!id) return <Page404 />;
   return (
     <div className="flex-1 flex flex-col overflow-hidden">
-      <FirmUpdateForm firmId={parseInt(id)} className="mx-5 lg:mx-10" />
+      <EnterpriseUpdateForm enterpriseId={parseInt(id)} className="mx-5 lg:mx-10" />
     </div>
   );
 }
--- src/pages/selling/new-quotation/index.tsx
+++ src/pages/selling/new-quotation/index.tsx
@@ -1,13 +1,5 @@
-import React from 'react';
-import { QuotationCreateForm } from '@/components/selling/quotation/QuotationCreateForm';
-import { useSearchParams } from 'next/navigation';
+import { QuotationCreateForm } from '@/components/invoicing/quotation/forms/QuotationCreateForm';
 
 export default function Page() {
-  const params = useSearchParams();
-  const firmId = params.get('firmId') || undefined;
-  return (
-    <div className="flex-1 flex flex-col overflow-hidden">
-      <QuotationCreateForm firmId={firmId} />
-    </div>
-  );
+  return <QuotationCreateForm />;
 }
